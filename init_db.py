9d89f860d91928213baeeaca4e22281a08e5b0a6
Sen <me@obsoletenerd.com>
2023-06-25 12:58:27 +1000

    Added some basic error handling

--- init_db.py
+++ init_db.py
@@ -5,65 +5,71 @@
 import os
 import psycopg2
 
-# Connect to the PostgreSQL database
-conn = psycopg2.connect(
-    # host="dpg-ci97vkh8g3ne2egtvuk0-a",
-    host="postgres://dpg-ci97vkh8g3ne2egtvuk0-a.singapore-postgres.render.com",
-    database="vicmadesql",
-    user=os.getenv("DB_USERNAME"),
-    password=os.getenv("DB_PASSWORD"),
-)
-cur = conn.cursor()
+try:
+    # Connect to the PostgreSQL database
+    conn = psycopg2.connect(
+        # host="dpg-ci97vkh8g3ne2egtvuk0-a",
+        host="postgres://dpg-ci97vkh8g3ne2egtvuk0-a.singapore-postgres.render.com",
+        database="vicmadesql",
+        user=os.getenv("DB_USRNAME"),
+        password=os.getenv("DB_PASSWORD"),
+    )
+except:
+    print("Could not connect to the PostgreSQL database.")
+    conn = None
 
-# Create some sample posts in the database
-print("Dropping existing tables to start fresh")
-cur.execute("DROP TABLE IF EXISTS posts;")
-cur.execute(
-    "CREATE TABLE posts (id serial PRIMARY KEY,"
-    "title varchar (150) NOT NULL,"
-    "repoid varchar (150) NOT NULL,"
-    "markdownurl varchar (256) NOT NULL,"
-    "linkurl varchar (256) NOT NULL,"
-    "content text,"
-    "date_added date DEFAULT CURRENT_TIMESTAMP);"
-)
+if conn != None:
+    cur = conn.cursor()
 
-print("Creating sample post")
-cur.execute(
-    "INSERT INTO posts (title, repoid, markdownurl, linkurl, content)"
-    "VALUES (%s, %s, %s, %s, %s)",
-    (
-        "Introduction to BallaratMade.com",
-        "BallaratMade.com",
-        "https://raw.githubusercontent.com/obsoletenerd/ballaratmade.com/main/_posts/2023-06-25-Introduction-to-BallaratMade.com.md",
-        "http://ballaratmade.com/",
-        "BallaratMade.com is an open-source git-backed multi-user blog that allows our local group of makers/hackers to share their projects and project updates in a single place. This post is a test post used in the development of the project, and will be replaced/updated later with more details.",
-    ),
-)
-
-# Create some sample Git repos to pull posts from
-print("Dropping existing repos")
-cur.execute("DROP TABLE IF EXISTS repos;")
-cur.execute(
-    "CREATE TABLE repos (id serial PRIMARY KEY,"
-    "owner varchar (150) NOT NULL,"
-    "repourl varchar (256) NOT NULL,"
-    "date_added date DEFAULT CURRENT_TIMESTAMP);"
-)
-print("Creating sample repos")
-approved_repos = {
-    "ObsoleteNerd.com": "obsoletenerd/obsoletenerd.github.io",
-    "BallaratMade.com": "obsoletenerd/vicmade.com",
-}
-
-for repo in approved_repos:
-    print("Adding %s" % repo)
+    # Create some sample posts in the database
+    print("Dropping existing posts...")
+    cur.execute("DROP TABLE IF EXISTS posts;")
     cur.execute(
-        "INSERT INTO repos (owner, repourl)" "VALUES (%s, %s)",
-        (repo, approved_repos[repo]),
+        "CREATE TABLE posts (id serial PRIMARY KEY,"
+        "title varchar (150) NOT NULL,"
+        "repoid varchar (150) NOT NULL,"
+        "markdownurl varchar (256) NOT NULL,"
+        "linkurl varchar (256) NOT NULL,"
+        "content text,"
+        "date_added date DEFAULT CURRENT_TIMESTAMP);"
     )
 
-conn.commit()
+    print("Inserting sample post...")
+    cur.execute(
+        "INSERT INTO posts (title, repoid, markdownurl, linkurl, content)"
+        "VALUES (%s, %s, %s, %s, %s)",
+        (
+            "Introduction to BallaratMade.com",
+            "BallaratMade.com",
+            "https://raw.githubusercontent.com/obsoletenerd/ballaratmade.com/main/_posts/2023-06-25-Introduction-to-BallaratMade.com.md",
+            "http://ballaratmade.com/",
+            "BallaratMade.com is an open-source git-backed multi-user blog that allows our local group of makers/hackers to share their projects and project updates in a single place. This post is a test post used in the development of the project, and will be replaced/updated later with more details.",
+        ),
+    )
 
-cur.close()
-conn.close()
+    # Create some sample Git repos to pull posts from
+    print("Dropping existing repos...")
+    cur.execute("DROP TABLE IF EXISTS repos;")
+    cur.execute(
+        "CREATE TABLE repos (id serial PRIMARY KEY,"
+        "owner varchar (150) NOT NULL,"
+        "repourl varchar (256) NOT NULL,"
+        "date_added date DEFAULT CURRENT_TIMESTAMP);"
+    )
+    print("Creating sample repos...")
+    approved_repos = {
+        "ObsoleteNerd.com": "obsoletenerd/obsoletenerd.github.io",
+        "BallaratMade.com": "obsoletenerd/vicmade.com",
+    }
+
+    for repo in approved_repos:
+        cur.execute(
+            "INSERT INTO repos (owner, repourl)" "VALUES (%s, %s)",
+            (repo, approved_repos[repo]),
+        )
+        print("Added %s!" % repo)
+
+    conn.commit()
+    print("DB reinitialised!")
+    cur.close()
+    conn.close()
--- refresh.py
+++ refresh.py
@@ -21,93 +21,99 @@ else:
 
 
 # Set up a connection to the database
-def get_db_connection():
-    conn = psycopg2.connect(
-        host=db_host,
-        database="vicmadesql",
-        user=os.getenv("DB_USERNAME"),
-        password=os.getenv("DB_PASSWORD"),
-    )
-    return conn
+try:
 
+    def get_db_connection():
+        conn = psycopg2.connect(
+            host=db_host,
+            database="vicmadesql",
+            user=os.getenv("DB_USERNAME"),
+            password=os.getenv("DB_PASSWORD"),
+        )
+        return conn
 
-conn = get_db_connection()
-cur = conn.cursor()
+    conn = get_db_connection()
+except:
+    print("Could not connect to the PostgreSQL database.")
+    conn = None
 
-# Get the list of approved repos from the database
-cur.execute("SELECT * FROM repos;")
-repos = cur.fetchall()
+if conn != None:
+    cur = conn.cursor()
 
-for repo in repos:
-    repo_name = repo[1]
-    print("Processing repo: %s" % repo_name)
+    # Get the list of approved repos from the database
+    cur.execute("SELECT * FROM repos;")
+    repos = cur.fetchall()
 
-    repo_url = repo[2]
-    print("Repo URL: %s" % repo_url)
+    for repo in repos:
+        repo_name = repo[1]
+        print("Processing repo: %s" % repo_name)
 
-    # Get the repo and pull the posts
-    repo = g.get_repo(repo_url)
+        repo_url = repo[2]
+        print("Repo URL: %s" % repo_url)
 
-    # TODO: Need error catching if posts dir doesn't exist
-    contents = repo.get_contents("_posts")
+        # Get the repo and pull the posts
+        repo = g.get_repo(repo_url)
 
-    # Loop through each file in the "_posts" directory
-    for content_file in contents:
-        if content_file.path.lower().endswith((".md")):  # Only markdown files
-            # Get the markdown so we can parse it and add the relevant parts to the database
-            post_markdown = repo.get_contents(content_file.path).decoded_content.decode(
-                "utf-8"
-            )
-            sections = post_markdown.split("---")
-            metadata = sections[1].strip()
-            metadata_lines = metadata.split("\n")
+        # TODO: Need error catching if posts dir doesn't exist
+        contents = repo.get_contents("_posts")
 
-            # Prepopulate with the user's root URL in case we cant find posturl later
-            post_url = repo_url
+        # Loop through each file in the "_posts" directory
+        for content_file in contents:
+            if content_file.path.lower().endswith((".md")):  # Only markdown files
+                # Get the markdown so we can parse it and add the relevant parts to the database
+                post_markdown = repo.get_contents(
+                    content_file.path
+                ).decoded_content.decode("utf-8")
+                sections = post_markdown.split("---")
+                metadata = sections[1].strip()
+                metadata_lines = metadata.split("\n")
 
-            # Build a title from the filename, in case they didn't put one in the metadata
-            post_title = content_file.name.split(".")[0].replace("-", " ")[11:]
+                # Prepopulate with the user's root URL in case we cant find posturl later
+                post_url = repo_url
 
-            for metadata_items in metadata_lines:
-                # Get the post title from the markdown metadata:
-                if metadata_items.split(":")[0] == "title":
-                    post_title = metadata_items.split(":")[1]
-                    print("Found Title: %s" % post_title)
+                # Build a title from the filename, in case they didn't put one in the metadata
+                post_title = content_file.name.split(".")[0].replace("-", " ")[11:]
 
-                # Get the post tags from the markdown metadata:
-                elif metadata_items.split(":")[0] == "tags":
-                    post_tags = metadata_items.split(":")[1]
-                    print("Found Tags: %s" % post_tags)
+                for metadata_items in metadata_lines:
+                    # Get the post title from the markdown metadata:
+                    if metadata_items.split(":")[0] == "title":
+                        post_title = metadata_items.split(":")[1]
+                        print("Found Title: %s" % post_title)
 
-                # Get the user's own post URL so we can link to the post on their blog
-                elif metadata_items.split(":")[0] == "posturl":
-                    post_url = metadata_items.split(":")[1]
-                    print("Found PostURL: %s" % post_url)
+                    # Get the post tags from the markdown metadata:
+                    elif metadata_items.split(":")[0] == "tags":
+                        post_tags = metadata_items.split(":")[1]
+                        print("Found Tags: %s" % post_tags)
 
-                # TODO: Add this to the DB structure for posts tied to a Github repository:
-                elif metadata_items.split(":")[0] == "github":
-                    post_github = metadata_items.split(":")[1]
-                    print("Found Github: %s" % post_github)
+                    # Get the user's own post URL so we can link to the post on their blog
+                    elif metadata_items.split(":")[0] == "posturl":
+                        post_url = metadata_items.split(":")[1]
+                        print("Found PostURL: %s" % post_url)
 
-                # TODO: Need to check if the above values are empty before trying to add
+                    # TODO: Add this to the DB structure for posts tied to a Github repository:
+                    elif metadata_items.split(":")[0] == "github":
+                        post_github = metadata_items.split(":")[1]
+                        print("Found Github: %s" % post_github)
 
-            # Everything left after the metadata is the content of the post:
-            post_content = sections[2]
+                    # TODO: Need to check if the above values are empty before trying to add
 
-            # Insert the post into the database:
-            cur.execute(
-                "INSERT INTO posts (title, repoid, markdownurl, linkurl, content)"
-                "VALUES (%s, %s, %s, %s, %s)",
-                (
-                    post_title,  # The title of the post pulled from the metadata
-                    repo_name,  # The user's website name/URL
-                    content_file.html_url,  # URL to the source Markdown file
-                    post_url,  # URL to the post on the user's blog
-                    post_content,  # The markdown content of the post
-                ),
-            )
+                # Everything left after the metadata is the content of the post:
+                post_content = sections[2]
 
-conn.commit()
+                # Insert the post into the database:
+                cur.execute(
+                    "INSERT INTO posts (title, repoid, markdownurl, linkurl, content)"
+                    "VALUES (%s, %s, %s, %s, %s)",
+                    (
+                        post_title,  # The title of the post pulled from the metadata
+                        repo_name,  # The user's website name/URL
+                        content_file.html_url,  # URL to the source Markdown file
+                        post_url,  # URL to the post on the user's blog
+                        post_content,  # The markdown content of the post
+                    ),
+                )
 
-cur.close()
-conn.close()
+    conn.commit()
+
+    cur.close()
+    conn.close()
